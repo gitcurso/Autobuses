a95eb858d0307a8e91ce739fa0f7b898a62ec338
Zeus <zeus@pue.es>
2016-05-23 11:29:36 +0200

    Se ha creado TurnoNoche

--- src/modelo/TurnoNoche.java
+++ src/modelo/TurnoNoche.java
@@ -1,13 +1,32 @@
 package modelo;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 /**
  * Created by poo2 on 17/05/2016.
  */
-public class TurnoNoche {
+public class TurnoNoche extends Turno {
 
-  private Turno busTurno;
+  // Atributos
 
-  public TurnoNoche() {
+  // normalizacion
+  private ArrayList<Incidencia> incidencias;
+
+
+  public TurnoNoche(Conductor conductor, LocalDate fecha,
+                    TipoTurno tipoTurno, Autobus bus) throws Exception{
+    super (conductor, fecha, tipoTurno, bus );
+
+    incidencias = new ArrayList<Incidencia>();
+  }
+
+  public void addIncidencia(Incidencia problema) throws Exception{
+    if (problema==null){
+      throw new Exception();
+    }
+
+    incidencias.add(problema);
 
   }
 }
